b7eaed200d9a3d26c750880e0ef9e0aaff8ad763
doer <499361328@qq.com>
2019-09-07 17:39:05 +0800

    ec-uni-app(第4节 App.vue引入全局公共样式) 1.引入官方css样式库(8分钟), 自定义代码提示块mylist

--- ec-uni-app/pages/index/index.vue
+++ ec-uni-app/pages/index/index.vue
@@ -1,8 +1,11 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+		<view class="uni-list">
+			<view class="uni-list-cell">
+				<view class="uni-list-cell-navigate uni-navigate-right">
+					
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
