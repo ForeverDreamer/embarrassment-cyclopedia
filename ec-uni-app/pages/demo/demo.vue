d1a7e6165735118aa5d4a8a156b50081912d2719
doer <499361328@qq.com>
2019-09-08 16:39:00 +0800

    ec-uni-app(第7节 uni-app和vuejs基础快速上手) 1.view和text组件和动画使用

---
+++ ec-uni-app/pages/demo/demo.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<view class="view-box" hover-class="view-box-hover animated rubberBand" hover-stay-time="800">
+			这是view
+		</view>
+		<text :selectable="true">这是text\n这是text\n这是text\n这是text\n这是text\n</text>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.view-box {
+	width: 200upx;
+	height: 200upx;
+	background: #007AFF;
+	color: #FFFFFF;
+	margin: 100upx;
+}
+.view-box-hover {
+	background: red;
+}
+</style>
--- ec-uni-app/pages/index/index.vue
+++ ec-uni-app/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		我的
+		糗事
 	</view>
 </template>
 
