5bb8dfd77ef7b6b79265032d9ed8234273889746
doer <499361328@qq.com>
2019-09-08 18:52:15 +0800

    ec-uni-app(第7节 uni-app和vuejs基础快速上手) 3.flex布局快速上手

--- ec-uni-app/pages/demo/demo.vue
+++ ec-uni-app/pages/demo/demo.vue
@@ -1,18 +1,13 @@
 <template>
 	<view>
 		
-		<view class="view-box" hover-class="view-box-hover animated rubberBand" hover-stay-time="800">
-			这是view
-		</view>
-		
-		<text :selectable="true">这是text\n这是text\n这是text\n这是text\n这是text\n</text>
-		
-		<view class="view-box" id="view2">css选择器</view>
-		
 		<view class="box">
-			<view>1</view>
-			<view>2</view>
-			<view>3</view>
+			<view class="box-item">1</view>
+			<view class="box-item">2</view>
+			<view class="box-item">3</view>
+			<view class="box-item">4</view>
+			<view class="box-item">5</view>
+			<view class="box-item">6</view>
 		</view>
 		
 	</view>
@@ -34,48 +29,33 @@
 
 <style>
 	
-.view-box {
-	width: 200upx;
-	height: 200upx;
-	background: #007AFF;
-	color: #FFFFFF;
-	margin: 100upx;
-}
-
-.view-box-hover {
-	background: red;
-}
-
-#view2, text {
-	background: yellow;
-}
-
 .box {
-	width: 300upx;
-	height: 300upx;
-	background: #007AFF;
+	width: 100%;
+	height: 500upx;
+	border: 1upx solid #CCCCCC;
+	display: flex;
+	flex-wrap: wrap;
+	/* flex-wrap: wrap-reverse; */
+	justify-content: center;
+	align-items: center;
+}
+
+.box-item {
 	color: #FFFFFF;
-	margin: 100upx;
-	padding: 50upx;
+	height: 200upx;
+	width: 200upx;
+	line-height: 200upx;
+	font-size: 30upx;
+	font-weight: bold;
+	text-align: center;
 }
 
-.box view {
-	font-size: 40upx;
+.box-item:nth-of-type(odd) {
+	background: #007AFF;
 }
 
-.box>view:nth-child(1) {
-	font-size: 40upx;
-	background: green;
-}
-
-.box>view:nth-child(2) {
-	font-size: 40upx;
-	background: red;
-}
-
-.box>view:nth-child(3) {
-	font-size: 40upx;
-	background: pink;
+.box-item:nth-of-type(even) {
+	background: #09BB07;
 }
 
 </style>
