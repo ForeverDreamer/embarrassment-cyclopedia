f75cf939fb3659c947ddb77ba3fc5f7bb846794e
doer <499361328@qq.com>
2019-09-16 19:25:23 +0800

    ec-ionic(第8节 首页开发) 10.优化图文列表组件(动画，关注，顶踩，点击进入详情页)

--- ec-ionic/src/app/embarrassment/components/index-item/index-item.component.ts
+++ ec-ionic/src/app/embarrassment/components/index-item/index-item.component.ts
@@ -1,4 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { ToastController } from '@ionic/angular';
+
 import { IndexItem } from './index-item.model';
 
 @Component({
@@ -9,8 +12,50 @@ import { IndexItem } from './index-item.model';
 export class IndexItemComponent implements OnInit {
   @Input() item: IndexItem;
 
-  constructor() { }
+  constructor(private toastCtrl: ToastController) { }
 
   ngOnInit() {}
 
+  // 关注
+  async onGuanzhu() {
+    this.item.isGuanzhu = true;
+    const toast = await this.toastCtrl.create({
+      message: '关注成功',
+      position: 'middle',
+      animated: true,
+      cssClass: 'my-toast',
+      duration: 2000
+    });
+    await toast.present();
+  }
+
+  // 顶踩
+  onCaozuo(type: string) {
+    console.log(type);
+    switch (type) {
+      case 'ding':
+        if ( this.item.infoNum.index === 1 ) { return; }
+        this.item.infoNum.dingNum++;
+        if ( this.item.infoNum.index === 2 ) {
+          this.item.infoNum.caiNum--;
+        }
+        this.item.infoNum.index = 1;
+        break;
+      case 'cai':
+        if ( this.item.infoNum.index === 2 ) { return; }
+        this.item.infoNum.caiNum++;
+        if ( this.item.infoNum.index === 1 ) {
+          this.item.infoNum.dingNum--;
+        }
+        this.item.infoNum.index = 2;
+        break;
+      default:
+        break;
+    }
+  }
+
+  // 进入详情页
+  openDetail() {
+    console.log('进入详情页');
+  }
 }
