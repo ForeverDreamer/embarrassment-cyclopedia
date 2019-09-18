7b4ddfd68b9d1e267ebbd85f94fac1dfcae4c200
doer <499361328@qq.com>
2019-09-18 18:18:22 +0800

    ec-uni-app(第13节关注列表页开发) 8.上拉加载功能实现

--- ec-uni-app/pages/news/news.vue
+++ ec-uni-app/pages/news/news.vue
@@ -9,10 +9,12 @@
 			@change="tabChange">
 				<!-- 关注 -->
 				<swiper-item>
-					<scroll-view scroll-y class="list">
-						<block v-for="(item, index) in list" :key="index">
+					<scroll-view scroll-y class="list" @scrolltolower="loadMore">
+						<block v-for="(item, index) in guanzhu.list" :key="index">
 							<common-list :item="item" :index="index"></common-list>
 						</block>
+						<!-- 上拉加载 -->
+						<loadMore :loadText="guanzhu.loadText"></loadMore>
 					</scroll-view>
 				</swiper-item>
 				<!-- 话题 -->
@@ -29,11 +31,13 @@
 <script>
 	import newsNavBar from "../../components/news/news-nav-bar.vue"
 	import commonList from "../../components/common/common-list.vue"
+	import loadMore from "../../components/common/load-more.vue"
 	
 	export default {
 		components: {
 			newsNavBar,
-			commonList
+			commonList,
+			loadMore
 		},
 		data() {
 			return {
@@ -43,78 +47,81 @@
 					{name: "关注", id: "guanzhu"},
 					{name: "话题", id: "topic"}
 				],
-				list: [
-					// 文字
-					{
-						userpic: "../../static/demo/userpic/12.jpg",
-						username: "哈哈",
-						sex: 0,  // 0 男, 1 女
-						age: 25,
-						isguanzhu: false,
-						title: "我是文字",
-						titlepic: "",
-						video: false,
-						share: false,
-						path: "深圳 龙岗",
-						sharenum: 20,
-						commentnum: 30,
-						goodnum: 20
-					},
-					// 图文
-					{
-						userpic: "../../static/demo/userpic/12.jpg",
-						username: "哈哈",
-						sex: 0,  // 0 男, 1 女
-						age: 25,
-						isguanzhu: false,
-						title: "我是图文",
-						titlepic: "../../static/demo/datapic/13.jpg",
-						video: false,
-						share: false,
-						path: "深圳 龙岗",
-						sharenum: 20,
-						commentnum: 30,
-						goodnum: 20
-					},
-					// 视频
-					{
-						userpic: "../../static/demo/userpic/12.jpg",
-						username: "哈哈",
-						sex: 0,  // 0 男, 1 女
-						age: 25,
-						isguanzhu: false,
-						title: "我是视频",
-						titlepic: "../../static/demo/datapic/13.jpg",
-						video: {
-							looknum: 20,  // 单位w(万)
-							long: "2:47"
+				guanzhu: {
+					loadText: "上拉加载更多",
+					list: [
+						// 文字
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "哈哈",
+							sex: 0,  // 0 男, 1 女
+							age: 25,
+							isguanzhu: false,
+							title: "我是文字",
+							titlepic: "",
+							video: false,
+							share: false,
+							path: "深圳 龙岗",
+							sharenum: 20,
+							commentnum: 30,
+							goodnum: 20
 						},
-						share: false,
-						path: "深圳 龙岗",
-						sharenum: 20,
-						commentnum: 30,
-						goodnum: 20
-					},
-					// 分享
-					{
-						userpic: "../../static/demo/userpic/12.jpg",
-						username: "哈哈",
-						sex: 0,  // 0 男, 1 女
-						age: 25,
-						isguanzhu: false,
-						title: "我是分享",
-						titlepic: "",
-						video: false,
-						share: {
-							title: "分享标题",
-							titlepic: "../../static/demo/datapic/14.jpg"
+						// 图文
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "哈哈",
+							sex: 0,  // 0 男, 1 女
+							age: 25,
+							isguanzhu: false,
+							title: "我是图文",
+							titlepic: "../../static/demo/datapic/13.jpg",
+							video: false,
+							share: false,
+							path: "深圳 龙岗",
+							sharenum: 20,
+							commentnum: 30,
+							goodnum: 20
 						},
-						path: "深圳 龙岗",
-						sharenum: 20,
-						commentnum: 30,
-						goodnum: 20
-					}
-				]
+						// 视频
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "哈哈",
+							sex: 0,  // 0 男, 1 女
+							age: 25,
+							isguanzhu: false,
+							title: "我是视频",
+							titlepic: "../../static/demo/datapic/13.jpg",
+							video: {
+								looknum: 20,  // 单位w(万)
+								long: "2:47"
+							},
+							share: false,
+							path: "深圳 龙岗",
+							sharenum: 20,
+							commentnum: 30,
+							goodnum: 20
+						},
+						// 分享
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "哈哈",
+							sex: 0,  // 0 男, 1 女
+							age: 25,
+							isguanzhu: false,
+							title: "我是分享",
+							titlepic: "",
+							video: false,
+							share: {
+								title: "分享标题",
+								titlepic: "../../static/demo/datapic/14.jpg"
+							},
+							path: "深圳 龙岗",
+							sharenum: 20,
+							commentnum: 30,
+							goodnum: 20
+						}
+					]
+				}	
 			}
 		},
 		onLoad() {
@@ -132,7 +139,37 @@
 			// 滑动事件
 			tabChange(e) {
 				this.tabIndex = e.detail.current;
-			}
+			},
+			// 上拉加载
+			loadMore() {
+				if (this.guanzhu.loadText != "上拉加载更多") {
+					return;
+				}
+				// 修改状态
+				this.guanzhu.loadText = "加载中...";
+				// 获取数据
+				setTimeout( () => {
+					// 获取完成
+					let obj = {
+						userpic: "../../static/demo/userpic/12.jpg",
+						username: "哈哈",
+						sex: 0,  // 0 男, 1 女
+						age: 25,
+						isguanzhu: false,
+						title: "我是图文",
+						titlepic: "../../static/demo/datapic/13.jpg",
+						video: false,
+						share: false,
+						path: "深圳 龙岗",
+						sharenum: 20,
+						commentnum: 30,
+						goodnum: 20
+					};
+					this.guanzhu.list.push(obj);
+					this.guanzhu.loadText = "上拉加载更多";
+				}, 1000)
+				// this.guanzhu.loadText = "没有更多数据了";
+			},
 		}
 	}
 </script>
