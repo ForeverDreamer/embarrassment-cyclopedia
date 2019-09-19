f7f241ed629eda9a48607d98a03e2c9795ba4998
doer <499361328@qq.com>
2019-09-19 18:10:43 +0800

    ec-uni-app(第16节 话题详情页开发) 3.封装话题介绍组件

---
+++ ec-uni-app/components/topic/topic-info.vue
@@ -0,0 +1,73 @@
+<template>
+	<view>
+		<!-- 模糊背景 -->
+		<view class="topic-bg">
+			<image :src="item.titlepic" mode="aspectFill" lazy-load></image>
+		</view>
+		<!-- 话题信息 -->
+		<view class="topic-info">
+			<view class="topic-info-t u-f-ac">
+				<image :src="item.titlepic" mode="widthFix" lazy-load></image>
+				<view>#{{item.title}}#</view>
+			</view>
+			<view class="topic-info-c u-f-ac">
+				<view>动态 {{item.totalnum}}</view>
+				<view>今日 {{item.todaynum}}</view>
+			</view>
+			<view class="topic-info-b">
+				{{item.desc}}
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			item: Object
+		}
+	}
+</script>
+
+<style scoped>
+	.topic-bg {
+		width: 100%;
+		height: 300rpx;
+		position: relative;
+		overflow: hidden;
+	}
+	.topic-bg image {
+		width: 100%;
+		position: absolute;
+		filter: blur(10px);
+	}
+	.topic-info {
+		padding: 0 25rpx;
+	}
+	.topic-info-t {
+		position: relative;
+	}
+	.topic-info-t>image{
+		width: 150rpx;
+		height: 150rpx;
+		border-radius: 20rpx;
+		position: absolute;
+		top: -75rpx
+	}
+	.topic-info-t>view{
+		font-size: 35rpx;
+		margin-left: 165rpx;
+		flex: 1;
+	}
+	.topic-info-c {
+		padding: 35rpx 0 15rpx 0;
+	}
+	.topic-info-c>view {
+		color: #CDCDCD;
+	}
+	.topic-info-b {
+		color: #A3A3A3;
+		font-size: 32rpx;
+		padding-bottom: 10rpx;
+	}
+</style>
--- ec-uni-app/pages/topic-detail/topic-detail.vue
+++ ec-uni-app/pages/topic-detail/topic-detail.vue
@@ -1,31 +1,26 @@
 <template>
 	<view>
-		<!-- 模糊背景 -->
-		<view class="topic-bg">
-			<image src="../../static/demo/topicpic/13.jpeg" mode="aspectFill" lazy-load></image>
-		</view>
-		<!-- 话题信息 -->
-		<view class="topic-info">
-			<view class="topic-info-t u-f-ac">
-				<image src="../../static/demo/topicpic/13.jpeg" mode="widthFix" lazy-load></image>
-				<view>#忆往事，敬余生#</view>
-			</view>
-			<view class="topic-info-c u-f-ac">
-				<view>动态 100</view>
-				<view>今日 100</view>
-			</view>
-			<view class="topic-info-b">
-				面试官：在电梯里巧遇马云你会怎么做？90后女孩的回答当初被录用
-			</view>
-		</view>
+		<!-- 话题介绍 -->
+		<topic-info :item="topicInfo"></topic-info>
 	</view>
 </template>
 
 <script>
+	import topicInfo from "../../components/topic/topic-info.vue"
+	
 	export default {
+		components: {
+			topicInfo
+		},
 		data() {
 			return {
-				
+				topicInfo: {
+					titlepic: "../../static/demo/topicpic/13.jpeg",
+					title: "忆往事，敬余生",
+					desc: "面试官：在电梯里巧遇马云你会怎么做？90后女孩的回答当初被录用",
+					totalnum: 100,
+					todaynum: 50
+				}
 			}
 		},
 		methods: {
@@ -35,44 +30,4 @@
 </script>
 
 <style>
-	.topic-bg {
-		width: 100%;
-		height: 300rpx;
-		position: relative;
-		overflow: hidden;
-	}
-	.topic-bg image {
-		width: 100%;
-		position: absolute;
-		filter: blur(10px);
-	}
-	.topic-info {
-		padding: 0 25rpx;
-	}
-	.topic-info-t {
-		position: relative;
-	}
-	.topic-info-t>image{
-		width: 150rpx;
-		height: 150rpx;
-		border-radius: 20rpx;
-		position: absolute;
-		top: -75rpx
-	}
-	.topic-info-t>view{
-		font-size: 35rpx;
-		margin-left: 165rpx;
-		flex: 1;
-	}
-	.topic-info-c {
-		padding: 35rpx 0 15rpx 0;
-	}
-	.topic-info-c>view {
-		color: #CDCDCD;
-	}
-	.topic-info-b {
-		color: #A3A3A3;
-		font-size: 32rpx;
-		padding-bottom: 10rpx;
-	}
 </style>
