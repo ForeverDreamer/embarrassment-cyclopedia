bced7a87fdd8001e176738fc7f4a74e013d98f08
doer <499361328@qq.com>
2019-09-20 18:07:30 +0800

    ec-uni-app(第18节 我的好友列表页开发) 2.tab导航组件再次优化

--- ec-uni-app/pages/user-list/user-list.vue
+++ ec-uni-app/pages/user-list/user-list.vue
@@ -1,14 +1,31 @@
 <template>
 	<view>
-		好友列表
+		<!-- tabbar切换 -->
+		<swiper-tab-head
+		:tabBars="tabBars" 
+		:tabIndex="tabIndex" 
+		@tabtap="tabtap"
+		scrollStyle="border-bottom: 0"
+		scrollItemStyle="width: 33%">
+		</swiper-tab-head>
 	</view>
 </template>
 
 <script>
+	import swiperTabHead from "../../components/index/swiper-tab-head.vue"
+
 	export default {
+		components: {
+			swiperTabHead
+		},
 		data() {
 			return {
-				
+				tabIndex: 0,
+				tabBars: [
+					{ name: "互关", id: "huguan", num: 10 },
+					{ name: "关注", id: "guanzhu", num: 20 },
+					{ name: "粉丝", id: "fensi", num: 30 },
+				]
 			}
 		},
 		// 监听导航按钮事件
@@ -20,7 +37,10 @@
 			}
 		},
 		methods: {
-			
+			// tabbar点击事件
+			tabtap(index) {
+				this.tabIndex = index;
+			}
 		}
 	}
 </script>
