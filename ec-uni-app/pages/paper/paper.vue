c9f4bd33f0ffaa6761210d197c6259c36a8ba82c
doer <499361328@qq.com>
2019-09-20 12:37:54 +0800

    ec-uni-app(第17节 小纸条列表页面开发) 4.下拉刷新功能实现

--- ec-uni-app/pages/paper/paper.vue
+++ ec-uni-app/pages/paper/paper.vue
@@ -62,8 +62,67 @@
 				]
 			}
 		},
+		// 监听下拉刷新
+		onPullDownRefresh() {
+			this.getdata();
+		},
 		methods: {
-			
+			// 获取数据
+			getdata() {
+				setTimeout( () => {
+					// 服务器获取数据
+					let arr = [
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "1111",
+							time: "20:21",
+							data: "赵丽颖《知否》一句话打脸，“读书...",
+							notenum: 5
+						},
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "2222",
+							time: "20:21",
+							data: "赵丽颖《知否》一句话打脸，“读书...",
+							notenum: 0
+						},
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "3333",
+							time: "20:21",
+							data: "赵丽颖《知否》一句话打脸，“读书...",
+							notenum: 10
+						},
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "JIA一勺",
+							time: "20:21",
+							data: "赵丽颖《知否》一句话打脸，“读书...",
+							notenum: 0
+						},
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "44444",
+							time: "20:21",
+							data: "赵丽颖《知否》一句话打脸，“读书...",
+							notenum: 19
+						},
+						{
+							userpic: "../../static/demo/userpic/12.jpg",
+							username: "5555",
+							time: "20:21",
+							data: "赵丽颖《知否》一句话打脸，“读书...",
+							notenum: 45
+						}
+					]
+					// 赋值
+					this.list = arr;
+					// 关闭下拉刷新
+					uni.stopPullDownRefresh();
+				}, 2000);
+				
+				
+			}
 		}
 	}
 </script>
