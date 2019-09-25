69006e5995f9ba63f86cad7803798cd4b3fe04ce
doer <499361328@qq.com>
2019-09-25 11:29:03 +0800

    ec-uni-app(第26节 编辑资料页面开发) 2.修改头像功能实现 3.showActionSheet接口使用

--- ec-uni-app/pages/user-set-userinfo/user-set-userinfo.vue
+++ ec-uni-app/pages/user-set-userinfo/user-set-userinfo.vue
@@ -2,22 +2,22 @@
 	<view class="body">
 		<view class="user-set-userinfo-list u-f-ac u-f-jsb">
 			<view>头像</view>
-			<view class="u-f-ac">
-				<image src="../../static/demo/userpic/11.jpg" mode="widthFix" lazy-load></image>
+			<view class="u-f-ac" @tap="changeimg">
+				<image :src="userpic" mode="aspectFill" lazy-load></image>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
 		<view class="user-set-userinfo-list u-f-ac u-f-jsb">
 			<view>昵称</view>
 			<view class="u-f-ac">
-				<view>哈哈</view>
+				<input type="text" v-model="username" />
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
 		<view class="user-set-userinfo-list u-f-ac u-f-jsb">
 			<view>性别</view>
-			<view class="u-f-ac">
-				<view>男</view>
+			<view class="u-f-ac" @tap="changeOne('sex')">
+				<view>{{sex}}</view>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
@@ -30,15 +30,15 @@
 		</view>
 		<view class="user-set-userinfo-list u-f-ac u-f-jsb">
 			<view>情感</view>
-			<view class="u-f-ac">
-				<view>未婚</view>
+			<view class="u-f-ac" @tap="changeOne('qg')">
+				<view>{{qg}}</view>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
 		<view class="user-set-userinfo-list u-f-ac u-f-jsb">
 			<view>职业</view>
-			<view class="u-f-ac">
-				<view>IT</view>
+			<view class="u-f-ac" @tap="changeOne('job')">
+				<view>{{job}}</view>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
@@ -56,13 +56,68 @@
 </template>
 
 <script>
+	let sexArr = ['不限', '男', '女']
+	let qgArr = ['未婚', '已婚', '不告诉你']
+	let jobArr = ['IT', '老师', '医生', "不告诉你"]
+	
 	export default {
 		data() {
 			return {
-				
+				userpic: "../../static/demo/userpic/11.jpg",
+				username: "哈哈哈",
+				sex: "不限",
+				qg: "未婚",
+				job: "IT"
 			}
 		},
 		methods: {
+			// 单列选择
+			changeOne(val) {
+				let arr = [];
+				switch(val) {
+					case 'sex':
+					arr = sexArr;
+						break;
+					case 'qg':
+					arr = qgArr;
+						break;
+					case 'job':
+					arr = jobArr;
+						break;
+				}
+				uni.showActionSheet({
+				    itemList: arr,
+				    success: function (res) {
+						console.log(arr[res.tapIndex]);
+						switch(val) {
+							case 'sex':
+							this.sex = arr[res.tapIndex];
+								break;
+							case 'qg':
+							this.qg = arr[res.tapIndex];
+								break;
+							case 'job':
+							this.job = arr[res.tapIndex];
+								break;
+						}
+				    },
+				    fail: function (res) {
+				        console.log(res.errMsg);
+				    }
+				});
+			},
+			// 修改头像
+			changeimg() {
+				uni.chooseImage({
+				    count: 1,
+				    sizeType: ['compressed'],
+				    success: function (res) {
+				        console.log(JSON.stringify(res.tempFilePaths));
+						console.log(res.tempFilePaths[0]);
+						this.userpic = res.tempFilePaths[0];
+				    }
+				});
+			},
 			submit() {
 				
 			}
@@ -85,6 +140,9 @@
 		width: 80rpx;
 		height: 80rpx;
 		border-radius: 100%;
+	}
+	.user-set-userinfo-list>view:last-child>input {
+		text-align: right;
 	}
 	.user-set-userinfo-list>view:last-child>view:last-of-type {
 		margin-left: 20rpx;
