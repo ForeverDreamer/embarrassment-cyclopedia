b62337f8747f847f64983efa7155433ac00f9f61
doer <499361328@qq.com>
2019-09-27 11:12:32 +0800

    ec-uni-app(第29节 登录页开发) 3.登录类型切换效果实现

--- ec-uni-app/pages/login/login.vue
+++ ec-uni-app/pages/login/login.vue
@@ -8,21 +8,28 @@
 		<image class="loginhead" src="../../static/common/loginhead.png" mode="widthFix" lazy-load></image>
 		<!-- 输入框+按钮 -->
 		<view class="body">
-			<!-- <input type="text" class="uni-input common-input" placeholder="昵称/手机号/邮箱" />
-			<view class="login-input-box">
-				<input type="text" class="uni-input common-input forget-input" placeholder="请输入密码" />
-				<view class="forget u-f-ajc">忘记密码</view>
-			</view> -->
-			<view class="login-input-box">
-				<view class="phone u-f-ajc">+86</view>
-				<input type="text" class="uni-input common-input phone-input" placeholder="输入手机号码" />
-			</view>
-			<view class="login-input-box">
-				<input type="text" class="uni-input common-input forget-input" placeholder="请输入验证码" />
-				<view class="forget u-f-ajc">
-					<view class="u-f-ajc yanzhengma login-font-color">获取验证码</view>
+			<!-- 账号密码登录 -->
+			<template v-if="!status">
+				<input type="text" class="uni-input common-input" placeholder="昵称/手机号/邮箱" />
+				<view class="login-input-box">
+					<input type="text" class="uni-input common-input forget-input" placeholder="请输入密码" />
+					<view class="forget u-f-ajc">忘记密码</view>
 				</view>
-			</view>
+			</template>
+			<!-- 手机验证码登录 -->
+			<template v-else>
+				<view class="login-input-box">
+					<view class="phone u-f-ajc">+86</view>
+					<input type="text" class="uni-input common-input phone-input" placeholder="输入手机号码" />
+				</view>
+				<view class="login-input-box">
+					<input type="text" class="uni-input common-input forget-input" placeholder="请输入验证码" />
+					<view class="forget u-f-ajc">
+						<view class="u-f-ajc yanzhengma login-font-color">获取验证码</view>
+					</view>
+				</view>
+			</template>
+			
 			<button class="user-set-btn" 
 			:loading="loading" 
 			:class="{'user-set-btn-disable':disabled}" 
@@ -30,8 +37,8 @@
 			</button>
 		</view>
 		<!-- 登录状态切换 -->
-		<view class="login-status u-f-ajc login-padding login-font-color">
-			验证码登录<view class="icon iconfont icon-jinru"></view>
+		<view class="login-status u-f-ajc login-padding login-font-color" @tap="changeStatus">
+			{{status?'账号密码登录':'验证码登录'}}<view class="icon iconfont icon-jinru"></view>
 		</view>
 		<!-- 第三方登录 -->
 		<view class="other-login-title u-f-ajc login-padding login-font-color">第三方登录</view>
@@ -54,11 +61,16 @@
 		},
 		data() {
 			return {
+				status: false,  // false账号密码登录，true手机验证码登录
 				disabled: true,
 				loading: false
 			}
 		},
 		methods: {
+			// 切换登录状态
+			changeStatus() {
+				this.status = !this.status;
+			},
 			// 返回上一步
 			back() {
 				console.log("返回上一步");
