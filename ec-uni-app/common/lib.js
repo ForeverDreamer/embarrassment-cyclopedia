3694e250799d46203eebc0f038edc89e58f3df31
doer <499361328@qq.com>
2019-09-28 16:41:58 +0800

    ec-uni-app(第31节 全局功能开发) 4.热更新

--- ec-uni-app/common/lib.js
+++ ec-uni-app/common/lib.js
@@ -33,7 +33,43 @@ const NetWork = {
 	}
 }
 
+// app更新
+const Update = function(){
+	// #ifdef APP-PLUS  
+	plus.runtime.getProperty(plus.runtime.appid, function(widgetInfo) {  
+	    uni.request({  
+	        url: 'http://www.example.com/update/',  
+	        data: {  
+	            version: widgetInfo.version,  
+	            name: widgetInfo.name  
+	        },  
+	        success: (result) => {  
+	            var data = result.data;  
+	            if (data.update && data.wgtUrl) {  
+	                uni.downloadFile({  
+	                    url: data.wgtUrl,  
+	                    success: (downloadResult) => {  
+	                        if (downloadResult.statusCode === 200) {  
+	                            plus.runtime.install(downloadResult.tempFilePath, {  
+	                                force: false  
+	                            }, function() {  
+	                                console.log('install success...');  
+	                                plus.runtime.restart();  
+	                            }, function(e) {  
+	                                console.error('install fail...');  
+	                            });  
+	                        }  
+	                    }  
+	                });  
+	            }  
+	        }  
+	    });  
+	});  
+	// #endif
+}
+
 import config from "./config.js"
 export default {
-	NetWork
+	NetWork,
+	Update
 }
