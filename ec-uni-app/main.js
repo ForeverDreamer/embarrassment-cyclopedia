6b101c1a20e71b88ba277b62cf086553556edad9
doer <499361328@qq.com>
2019-09-28 16:26:24 +0800

    ec-uni-app(第31节 全局功能开发) 3.监听网络状态

--- ec-uni-app/main.js
+++ ec-uni-app/main.js
@@ -7,10 +7,14 @@ Vue.config.productionTip = false
 import config from "./common/config.js"
 Vue.prototype.config = config
 
+import lib from "./common/lib.js"
+Vue.prototype.lib=lib
+
 App.mpType = 'app'
 
 const app = new Vue({
     ...App,
-	config
+	config,
+	lib
 })
 app.$mount()
