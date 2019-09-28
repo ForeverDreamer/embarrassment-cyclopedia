155b0294a5540e8e86080c911b917fb2ea99259e
doer <499361328@qq.com>
2019-09-28 12:08:59 +0800

    ec-uni-app(第30节 个人空间开发) 5.个人空间UI界面实现(三)

--- ec-uni-app/pages/user-space/user-space.vue
+++ ec-uni-app/pages/user-space/user-space.vue
@@ -17,7 +17,19 @@
 		scrollStyle="border-bottom:0;">
 		</swiper-tab-head>
 		<!-- 主页 -->
-		<user-space-userinfo :userinfo="userinfo"></user-space-userinfo>
+		<block v-for="(item,index) in tablist" :key="index">
+			<template v-if="tabIndex==0">
+				<user-space-userinfo :userinfo="userinfo"></user-space-userinfo>
+			</template>
+			<template v-else-if="tabIndex==index">
+				<!-- 列表 -->
+				<block v-for="(list,listindex) in item.list" :key="listindex">
+					<common-list :item="list" :index="listindex"></common-list>
+				</block>
+				<!-- 上拉加载 -->
+				<load-more :loadText="item.loadText"></load-more>
+			</template>
+		</block>
 	</view>
 </template>
 
@@ -26,13 +38,17 @@
 	import homeData from "../../components/home/home-data.vue";
 	import swiperTabHead from "../../components/index/swiper-tab-head.vue";
 	import userSpaceUserinfo from "../../components/user-space/user-space-userinfo.vue"
+	import commonList from "../../components/common/common-list.vue";
+	import loadMore from "../../components/common/load-more.vue";
 	
 	export default {
 		components: {
 			userSpaceHead,
 			homeData,
 			swiperTabHead,
-			userSpaceUserinfo
+			userSpaceUserinfo,
+			commonList,
+			loadMore
 		},
 		data() {
 			return {
@@ -60,11 +76,199 @@
 					{ name:"主页",id:"zhuye" },
 					{ name:"糗事",id:"qiushi" },
 					{ name:"动态",id:"dongtai" },
+				],
+				tablist: [
+					{},
+					{
+						loadText: "上拉加载更多",
+						list: [
+							// 文字
+							{
+								userpic: "../../static/demo/userpic/12.jpg",
+								username: "哈哈",
+								sex: 0,  // 0 男, 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是文字",
+								titlepic: "",
+								video: false,
+								share: false,
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							},
+							// 图文
+							{
+								userpic: "../../static/demo/userpic/12.jpg",
+								username: "哈哈",
+								sex: 0,  // 0 男, 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是图文",
+								titlepic: "../../static/demo/datapic/13.jpg",
+								video: false,
+								share: false,
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							},
+							// 视频
+							{
+								userpic: "../../static/demo/userpic/12.jpg",
+								username: "哈哈",
+								sex: 0,  // 0 男, 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是视频",
+								titlepic: "../../static/demo/datapic/13.jpg",
+								video: {
+									looknum: 20,  // 单位w(万)
+									long: "2:47"
+								},
+								share: false,
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							},
+							// 分享
+							{
+								userpic: "../../static/demo/userpic/12.jpg",
+								username: "哈哈",
+								sex: 0,  // 0 男, 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是分享",
+								titlepic: "",
+								video: false,
+								share: {
+									title: "分享标题",
+									titlepic: "../../static/demo/datapic/14.jpg"
+								},
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							}
+						]
+					},
+					{
+						loadText: "上拉加载更多",
+						list: [
+							// 文字
+							{
+								userpic: "../../static/demo/userpic/9.jpg",
+								username: "嘿嘿",
+								sex: 0,  // 0 男, 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是文字",
+								titlepic: "",
+								video: false,
+								share: false,
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							},
+							// 图文
+							{
+								userpic: "../../static/demo/userpic/9.jpg",
+								username: "嘿嘿",
+								sex: 0,  // 0 男, 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是图文",
+								titlepic: "../../static/demo/datapic/14.jpg",
+								video: false,
+								share: false,
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							},
+							// 视频
+							{
+								userpic: "../../static/demo/userpic/9.jpg",
+								username: "嘿嘿",
+								sex: 0,  // 0 男, 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是视频",
+								titlepic: "../../static/demo/datapic/14.jpg",
+								video: {
+									looknum: 20,  // 单位w(万)
+									long: "2:47"
+								},
+								share: false,
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							},
+							// 分享
+							{
+								userpic: "../../static/demo/userpic/9.jpg",
+								username: "嘿嘿",
+								sex: 0,  // 0 男, 1 女
+								age: 25,
+								isguanzhu: false,
+								title: "我是分享",
+								titlepic: "",
+								video: false,
+								share: {
+									title: "分享标题",
+									titlepic: "../../static/demo/datapic/11.jpg"
+								},
+								path: "深圳 龙岗",
+								sharenum: 20,
+								commentnum: 30,
+								goodnum: 20
+							}
+						]
+					},
 				]
 			}
 		},
+		// 上拉触底事件
+		onReachBottom() {
+			// 上拉加载
+			this.loadMore();
+		},
 		methods: {
+			loadMore() {
+				if (this.tablist[this.tabIndex].loadText != "上拉加载更多") {
+					return;
+				}
+				// 修改状态
+				this.tablist[this.tabIndex].loadText = "加载中...";
+				// 获取数据
+				setTimeout( () => {
+					// 获取完成
+					let obj = {
+						userpic: "../../static/demo/userpic/12.jpg",
+						username: "哈哈",
+						sex: 0,  // 0 男, 1 女
+						age: 25,
+						isguanzhu: false,
+						title: "我是图文",
+						titlepic: "../../static/demo/datapic/13.jpg",
+						video: false,
+						share: false,
+						path: "深圳 龙岗",
+						sharenum: 20,
+						commentnum: 30,
+						goodnum: 20
+					};
+					this.tablist[this.tabIndex].list.push(obj);
+					this.tablist[this.tabIndex].loadText = "上拉加载更多";
+				}, 1000)
+				// this.tablist[this.tabIndex].loadText = "没有更多数据了";
+			},
 			tabtap(index){
+				// console.log(index);
 				this.tabIndex=index;
 				// this.tablist[this.tabIndex].page = 1;
 				// this.getList();
