f558498db6b6c178718545cb69d36f116c8caa95
doer <499361328@qq.com>
2019-09-28 15:40:34 +0800

    ec-uni-app(第31节 全局功能开发) 1.清除缓存功能

--- ec-uni-app/components/home/home-list-item.vue
+++ ec-uni-app/components/home/home-list-item.vue
@@ -20,10 +20,23 @@
 			clickevent() {
 				switch (this.item.clicktype){
 					case "navigateTo":
-					if (this.item.url) { uni.navigateTo({ url: this.item.url }); }
+						if (this.item.url) { uni.navigateTo({ url: this.item.url }); }
 						break;
 					case "switchTab":
-					if (this.item.url) { uni.switchTab({ url: this.item.url }); }
+						if (this.item.url) { uni.switchTab({ url: this.item.url }); }
+						break;
+					case "clear":
+						uni.showModal({
+							title: '提示',
+							content: '是否要清除缓存？',
+							confirmText: '立刻清除',
+							success: res => {
+								if(res.confirm){
+									uni.clearStorage();
+									uni.showToast({ title: '清除缓存成功！'});
+								}
+							},
+						});
 						break;
 				}
 			}
